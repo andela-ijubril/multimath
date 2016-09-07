f57b19e1f1b9dd73a46b44ca1bcdc6cca7b90730
Jubril Issa <issa.jubril@andela.com>
2016-09-07 07:33:05 +0100

    add basic javascript functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
+// You can pass argument like this
 (function (name) {
   console.log("Inside my cool IIFE function");
   console.log("Hello " + name);
 })("Jubril");
+
+(function () {
+  console.log("Starting MultiMath with an IIFE");
+
+  // add click handler to the start game button
+  document.getElementById('startGame').addEventListener('click', function () {
+    player.setName(document.getElementById('playerName').value);
+    game.printGame();
+  });
+
+  // add click handler to calculate score button
+  document.getElementById('calculate').addEventListener('click', function () {
+    game.calculateScore();
+  });
+
+  // set the default number of problems
+  document.getElementById('problemCount').value = game.getProblemCount();
+})();
